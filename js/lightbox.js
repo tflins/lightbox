19ba534842b0b6885ee714541a8c5bb5820862bf
tflins <itan.flin@gmail.com>
2018-09-09 22:06:57 +0800

    修复JS渲染DOM结构

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,17 +4,25 @@
         var self = this;
 
         // 创建遮罩层和弹出层
-        this.popupMask = "<div id='G-lightbox-mask'></div>";
-        this.popupWin = "<div id='G-lightbox-popup'>";
+        this.popupMask = document.createElement("div");
+        this.popupWin = document.createElement("div");;
 
         // 保存body
         this.bodyNode = document.body;
 
+        // 初始化Lightbox
+        this.init();
         // 渲染剩余的DOM，并插入到body中
         this.renderDom();
     }
     // Lightbox的原型对象
     Lightbox.prototype = {
+        // 初始化方法
+        init: function() {
+            this.popupMask.setAttribute("id", "G-lightbox-mask");
+            this.popupWin.setAttribute("id", "G-lightbox-popup");
+        },
+        // 渲染其他DOM方法
         renderDom: function() {
             var strDom = "<div class='lightbox-pic-view'>" +
                             "<span class='lightbox-btn lightbox-prev-btn lightbox-prev-btn-show'></span>" +
@@ -27,11 +35,10 @@
                                 "<span class='lightbox-of-index'>当前索引：1 / 4</span>" +
                             "</div>" +
                             "<span class='lightbox-close-btn'></span>" +
-                        "</div>" +
                         "</div>";
             // 插入到弹出层中
-            this.popupWin += strDom;
-            // 并插入到body中
+            this.popupWin.innerHTML = strDom;
+            // 将遮罩层和弹出层添加到body中
             this.bodyNode.append(this.popupMask, this.popupWin);
         }
     }
